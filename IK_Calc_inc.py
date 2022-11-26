85bcca57b6b31c83187d7eb7c5ad108a15304b83
Tosse <74308704+Traz3rt@users.noreply.github.com>
2022-11-26 04:31:42 +0100

    Update and rename IK_Calc.py to IK_Calc_inc.py

--- IK_Calc_inc.py
+++ IK_Calc_inc.py
@@ -1,16 +1,7 @@
-# Type help("robodk.robolink") or help("robodk.robomath") for more information
-# Press F5 to run the script
-# Documentation: https://robodk.com/doc/en/RoboDK-API.html
-# Reference:     https://robodk.com/doc/en/PythonAPI/robodk.html
-# Note: It is not required to keep a copy of this file, your Python script is saved with your RDK project
-# You can also use the new version of the API:
-from robodk import robolink    # RoboDK API
-from robodk import robomath    # Robot toolbox
 
-# Forward and backwards compatible use of the RoboDK API:
-# Remove these 2 lines to follow python programming guidelines
 from robodk import *      # RoboDK API
 from robolink import *    # Robot toolbox
+
 # Link to RoboDK
 sim = Robolink()
 math = robomath
@@ -59,11 +50,6 @@ def Oppgave3():
     
 def IK ():
     return
-
-
-
-
-
 
 H06, H03, H36 = Oppgave3()
 #print(H36)
